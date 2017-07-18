439d960e5f110a879048949a3de9ad010e9eebae
galynag <g.alenicheva@gmail.com>
2017-07-18 18:16:27 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,18 @@
 
 
-var t=setTimeout(myClock,100);
+var t=setTimeout(myClock,1000);
 var s=0;
 var m=0;
 function myClock() {
-	var spanMin = document.getElementById('min');
-	var spanSec = document.getElementById('sec');
-  s=s+6;
-  spanSec.style.transform='rotate('+s+'deg)';
+    var spanMin = document.getElementById('min');
+    var spanSec = document.getElementById('sec');
+    s=s+6;
+    spanSec.style.transform='rotate('+s+'deg)';
 
     if ((s % 360) == 0) {
-      m=m+6;
-      spanMin.style.transform='rotate('+m+'deg)';
-      setTimeout(myClock,30)
-  }
-    else { setTimeout(myClock,100) }
-
+        m=m+6;
+        spanMin.style.transform='rotate('+m+'deg)';
+        setTimeout(myClock,1000)
+    }
+    else { setTimeout(myClock,1000) }
 }
